d840166199cb86ffd5b8875089f962be64d8b558
Rithk <rithkchaudhary86@gmail.com>
2024-02-07 00:30:10 +0530

    postgres

---
+++ postgres/src/index.ts
@@ -0,0 +1,27 @@
+// write a function to create a users table in your database.
+import { Client } from 'pg'
+ 
+const client = new Client({
+    host: 'localhost',
+    port: 5432,
+    database: 'test2',
+    user: 'postgres',
+    password: '9864',
+  })
+  
+
+async function createUsersTable() {
+    await client.connect()
+    const result = await client.query(`
+        CREATE TABLE users (
+            id SERIAL PRIMARY KEY,
+            username VARCHAR(50) UNIQUE NOT NULL,
+            email VARCHAR(255) UNIQUE NOT NULL,
+            password VARCHAR(255) NOT NULL,
+            created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
+        );
+    `)
+    console.log(result)
+}
+
+createUsersTable();
